5bb21f851483c5ac6af2ea75378778592d823d42
MightyK <KMightyDev@gmail.com>
2021-03-10 22:32:41 -0500

    Hover functionality initialized

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+    const [headingText, setHeadingText] = useState("Hello");
+
+    const handleClick = () =>{
+        setHeadingText("Submitted");
+    }
+
+    const [color, setColor] = useState({backgroundColor: "white"});
+
+    const onHover = () => {
+        setColor({backgroundColor: "black"});
+    }
+
+    const onOut = () => {
+        setColor({backgroundColor: "white"});
+    }
+
     return (
         <div className='container'>
-            <h1>Hello</h1>
+            <h1>{headingText}</h1>
             <input type='text' placeholder="What's your name?" />
-            <button>Submit</button>
+            <button style={color} onMouseOver={onHover} onMouseOut={onOut} onClick={handleClick}>Submit</button>
         </div>
     );
 }
