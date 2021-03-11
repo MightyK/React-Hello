a346903ce16ced9a32a4805c2f454e3db39f4fa7
MightyK <KMightyDev@gmail.com>
2021-03-10 23:11:02 -0500

    Input interactivity initialized

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,27 +1,46 @@
+/* eslint-disable no-restricted-globals */
 import React, {useState} from 'react';
 
 const App = () => {
-    const [headingText, setHeadingText] = useState("Hello");
-
-    const handleClick = () =>{
-        setHeadingText("Submitted");
-    }
-
+    const [name, setName] = useState("");
+    const [headingText, setHeadingText] = useState(`Hello`);
     const [color, setColor] = useState({backgroundColor: "white"});
-
+    
+    const handleClick = () =>{
+        setHeadingText(`Hello ${name}!`);
+    }
+    
+    
     const onHover = () => {
         setColor({backgroundColor: "black"});
     }
-
+    
     const onOut = () => {
         setColor({backgroundColor: "white"});
     }
-
+    
+    const handleChange = () => {
+        setName(event.target.value);
+    }
+    
     return (
         <div className='container'>
             <h1>{headingText}</h1>
-            <input type='text' placeholder="What's your name?" />
-            <button style={color} onMouseOver={onHover} onMouseOut={onOut} onClick={handleClick}>Submit</button>
+            <input 
+                onChange={handleChange}
+                type='text' 
+                placeholder="What's your name?"
+                value={name} 
+            />
+
+            <button 
+                style={color} 
+                onMouseOver={onHover} 
+                onMouseOut={onOut} 
+                onClick={handleClick}
+            >
+                Submit
+            </button>
         </div>
     );
 }
